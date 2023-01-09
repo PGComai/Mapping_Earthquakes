3455101a25425a1c2c255f5244a3c001fd3e103b
PGComai <comai.alex@gmail.com>
2023-01-09 14:31:05 -0800

    getting there

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,7 +1,7 @@
 console.log("roll out");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([51.505, -0.09], 13);
+let map = L.map('mapid').setView([41.8781, -87.6298], 4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -12,3 +12,15 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+let cityData = cities;
+
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup('<h2>' + city.city + ', ' + city.state + 
+    '</h2> <hr> <h3>Population ' + city.population.toLocaleString() + '</h3>')
+    .addTo(map);
+});
